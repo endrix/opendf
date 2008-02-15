d3b9141cc144d8fcc83ba95175fffd9a2acb6cac
jwjanneck <jwjanneck@4d97daf9-442b-0410-bea3-872e0d7a45eb>
2008-02-15 04:38:04 +0000

    Fixed bug in NL parser---NL now allows types with parameters.

--- core/source/java/net/sf/opendf/cli/PhasedSimulator.java
+++ core/source/java/net/sf/opendf/cli/PhasedSimulator.java
@@ -344,6 +344,7 @@ public class PhasedSimulator {
                     return ERRORLIMIT;
                 }
             }
+            done = true;
         }
         catch (Throwable t)
         {
