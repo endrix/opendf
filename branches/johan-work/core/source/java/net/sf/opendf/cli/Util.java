75509265774a9ebbcff58341b978bab81ca52c2f
ekeroid <ekeroid@4d97daf9-442b-0410-bea3-872e0d7a45eb>
2009-03-31 12:43:51 +0000

    Fixed bug in evaluating parameter expressions

--- branches/johan-work/core/source/java/net/sf/opendf/cli/Util.java
+++ branches/johan-work/core/source/java/net/sf/opendf/cli/Util.java
@@ -149,8 +149,9 @@ public class Util {
 		"net/sf/opendf/transforms/xdfBuildXMLAttribute.xslt",
 		"net/sf/opendf/transforms/xdfFoldAttributes.xslt",
         
-		"net/sf/opendf/cal/transforms/xlim/AddDirectives.xslt"
-    };
+		"net/sf/opendf/cal/transforms/xlim/AddDirectives.xslt",
+		"net/sf/opendf/cal/transforms/EvaluateNetworkExpressions.xslt"
+	};
     private static final String [] inlineTransforms = { "net/sf/opendf/cal/transforms/Inline.xslt" };
     private static final String [] postInlineTransforms = {
         "net/sf/opendf/cal/transforms/AddID.xslt",
