7d59171ebfb75a96aa39f7cb1c668e908335dbc4
carlvp <carlvp@4d97daf9-442b-0410-bea3-872e0d7a45eb>
2009-05-08 08:14:26 +0000

    removed xlimdiff branch

--- branches/xlimdiff/contrib/Cal2C/cal2c.bat
+++
@@ -1,5 +0,0 @@
-set SDL="D:\libraries\SDL-1.2.13"
-set SYSC="D:\libraries\systemc-2.2.0"
-set TLM="D:\libraries\TLM-2007-11-29"
-src\_build\Cal2C\cal2c.native -debug -I %SDL% -I testbench/libcal -I %SYSC% -I %TLM% -mp examples/RVC_MPEG4_SP_Decoder -o testbench/generated testbed
-pause
--- branches/xlimdiff/core/source/java/net/sf/opendf/cal/i2/types/NumericType.java
+++
@@ -1,7 +0,0 @@
-package net.sf.opendf.cal.i2.types;
-
-
-
-public interface NumericType extends Type {
-
-}
