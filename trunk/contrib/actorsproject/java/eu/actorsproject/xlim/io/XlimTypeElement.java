789e28b26bd1bcf2f61fe74411732c8c6dffdfe6
carlvp <carlvp@4d97daf9-442b-0410-bea3-872e0d7a45eb>
2010-04-16 07:16:55 +0000

    Fixed/rolled back changes in XLIM reader (relating to type definitions)

--- trunk/contrib/actorsproject/java/eu/actorsproject/xlim/io/XlimTypeElement.java
+++ trunk/contrib/actorsproject/java/eu/actorsproject/xlim/io/XlimTypeElement.java
@@ -37,12 +37,15 @@
 
 package eu.actorsproject.xlim.io;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import eu.actorsproject.util.XmlAttributeFormatter;
 import eu.actorsproject.util.XmlElement;
 import eu.actorsproject.xlim.XlimType;
 import eu.actorsproject.xlim.XlimTypeArgument;
+import eu.actorsproject.xlim.XlimTypeKind;
 
 
 /**
@@ -50,19 +53,169 @@ import eu.actorsproject.xlim.XlimTypeArgument;
  * XlimTypeElements are used only temporarily when reading and writing XLIM files.
  * They are then converted into XlimTypes.
  */
-public abstract class XlimTypeElement implements XmlElement {
+public class XlimTypeElement implements XmlElement {
+
+	private String mName;
+	private XlimLocation mLocation;
+	private List<TypeArgElement> mTypeArgs;
+	private XlimType mType;
+	
+	/**
+	 * Create a (yet unresolved) type element
+	 * @param name
+	 * @param location
+	 */
+	public XlimTypeElement(String name, XlimLocation location) {
+		mName=name;
+		mLocation=location;
+		mTypeArgs=new ArrayList<TypeArgElement>();
+	}
+	
+	/**
+	 * Create a (resolved) type element
+	 * @param typeDef
+	 */
+	public XlimTypeElement(XlimTypeDef typeDef) {
+		mName=typeDef.getName();
+		mType=typeDef.createType();
+		mTypeArgs=Collections.emptyList();
+	}
+	
+	/**
+	 * Create a (resolved) type element
+	 * @param type
+	 */
+	public XlimTypeElement(XlimType type) {
+		mName=type.getTypeName();
+		mType=type;
+		mTypeArgs=new ArrayList<TypeArgElement>();
+	}
 
 	/**
-	 * @param readerPlugIn  plug-in used to create types
-	 * @param context       context, which is used to resolve typeDefs
-	 * @return the type that corresponds to this element
+	 * Adds a value parameter to this type element
+	 * @param name  the name of the (formal) parameter
+	 * @param value the actual value of the parameter
 	 */
-	public abstract XlimType createType(ReaderPlugIn readerPlugIn, ReaderContext context);
+	public void addValuePar(String name, String value) {
+		mTypeArgs.add(new ValueParElement(name,value));
+	}
+
+	/**
+	 * Adds a type parameter to this type element
+	 * @param name         the name of the (formal) parameter
+	 * @param typeElement  the actual value of the parameter
+	 */
+	public void addTypePar(String name, XlimTypeElement typeElement) {
+		mTypeArgs.add(new TypeParElement(name,typeElement));
+	}
+
+	public String getName() {
+		return mName;
+	}
 
 	/**
 	 * @return type of this element (null if there are unresolved references)
 	 */
-	public abstract XlimType getType();
+	public XlimType getType() {
+		return mType;
+	}
+
+		
+	/**
+	 * @param readerPlugIn  plug-in used to create types
+	 * @param context       context, which is used to resolve typeDefs (optional, may be null)
+	 * @return the type that corresponds to this element
+	 * 
+	 * Using a null context, typeDefs cannot be resolved
+	 */
+	public XlimType createType(ReaderPlugIn readerPlugIn, ReaderContext context) {
+		if (mType==null) {
+			XlimTypeDef typeDef=(context!=null)? context.getTypeDef(mName) : null;
+		
+			if (typeDef!=null) {
+				if (mTypeArgs.isEmpty())
+					mType=typeDef.createType();
+				else
+					throw new XlimReaderError("Unexpected parameters to typeDef", mLocation);
+			}
+			else {
+				XlimTypeKind typeConstructor=readerPlugIn.getTypeKind(mName);
+				assert(typeConstructor!=null);
+				ArrayList<XlimTypeArgument> arguments=new ArrayList<XlimTypeArgument>();
+				for (TypeArgElement element: mTypeArgs) {
+					XlimTypeArgument arg=element.createTypeArgument(readerPlugIn, context);
+					if (arg!=null)
+						arguments.add(arg);
+					else
+						return null; // No point in proceeding here (error is reported)
+				}
+
+				mType=typeConstructor.createType(arguments);
+			}
+		}
+		
+		return mType;
+	}
+	
+	@Override
+	public String getTagName() {
+		return "type";
+	}
+		
+	@Override
+	public Iterable<? extends XmlElement> getChildren() {
+		return mTypeArgs;
+	}
+
+	@Override
+	public String getAttributeDefinitions(XmlAttributeFormatter formatter) {
+		return "name=\"" + mName + "\"";
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof XlimTypeElement) {
+			XlimTypeElement other=(XlimTypeElement) o;
+			
+			if (mName.equals(other.mName)) {
+				// First check that all arguments of this type element
+				// have a corresponding type element in the other one
+				for (TypeArgElement arg1: mTypeArgs) {
+					TypeArgElement arg2=other.find(arg1.getName());
+					if (arg2==null || arg1.equals(arg2)==false)
+						return false;
+				}
+				
+				// Then check that the other type element has no extra arguments
+				for (TypeArgElement arg2: other.mTypeArgs) {
+					if (find(arg2.getName())==null)
+						return false;
+				}
+				
+				// Ok, they are equal
+				return true;
+			}
+			else
+				return false;
+		}
+		else
+			return false;
+	}
+	
+	TypeArgElement find(String name) {
+		for (TypeArgElement arg: mTypeArgs)
+			if (name.equals(arg.getName()))
+				return arg;
+		return null; // Not found
+	}
+	
+	@Override 
+	public int hashCode() {
+		int h=mName.hashCode();
+		for (TypeArgElement arg: mTypeArgs)
+			h += arg.hashCode(); // Order of argument doesn't matter
+		return h;
+	}
 }
 
 abstract class TypeArgElement implements XmlElement {
