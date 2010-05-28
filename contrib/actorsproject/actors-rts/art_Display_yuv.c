133f12817c0b848e53e691223d00f9dad610df30
carlvp <carlvp@4d97daf9-442b-0410-bea3-872e0d7a45eb>
2010-05-28 09:19:23 +0000

    A bit of clean-up in art_Display_yuv

--- contrib/actorsproject/actors-rts/art_Display_yuv.c
+++ contrib/actorsproject/actors-rts/art_Display_yuv.c
@@ -52,25 +52,22 @@ typedef struct {
   yuv_sample_t          macroBlock[MB_SIZE];
   int                   mbx,mby;
   int                   count;
-  int                   comp;
-  int                   start;
   int                   startTime;
-  int                   now;
-  int                   lastFrames;
+  int                   now;         // TBD: Only used in RM-version (remove?)
+  int                   lastFrames;  // TBD: Only used in RM-version (remove?)
   int                   totFrames;
-  int                   fp;
-  int                   ppf;
   FrameBuffer           frameBuffer;
 } ActorInstance_art_Display_yuv;
 
 #define IN0_In               ART_INPUT(0)
 
-static const int exitcode_block_In_1[] = {
-  EXITCODE_BLOCK(1), 0, 1
+static const int exitcode_block_In_64[] = {
+  EXITCODE_BLOCK(1), 0, 64
 };
 
+// TBD: Only used in RM-version (remove?)
 static const int exitcode_block_Out_1[] = {
-  EXITCODE_BLOCK(1), 0, 1
+  EXITCODE_BLOCK(1), 1, 1
 };
 
 ART_ACTION_SCHEDULER(art_Display_yuv_action_scheduler)
@@ -78,9 +75,7 @@ ART_ACTION_SCHEDULER(art_Display_yuv_action_scheduler)
   const int *result = EXIT_CODE_YIELD;
   ActorInstance_art_Display_yuv *thisActor=
     (ActorInstance_art_Display_yuv *) pBase;
-  int32_t start=thisActor->start;
   int32_t count=thisActor->count;
-  int32_t comp=thisActor->comp;
 #ifdef RM
   ART_ACTION_SCHEDULER_ENTER(1, 1);
 #else
@@ -90,69 +85,33 @@ ART_ACTION_SCHEDULER(art_Display_yuv_action_scheduler)
   ART_ACTION_SCHEDULER_LOOP {
     ART_ACTION_SCHEDULER_LOOP_TOP;
     do {
-      do {
-	while (count<64) {
-	  /*** read action ***/ 
-	  int32_t avail=pinAvailIn_int32_t(IN0_In);
-	  int32_t n;
-	  yuv_sample_t *ptr;
-	  yuv_sample_t *end;
+      while (count<384) {
+	/*** read action ***/ 
+	int32_t avail=pinAvailIn_int32_t(IN0_In);
+	yuv_sample_t *ptr;
 
-	  if (avail==0) {
-	    // Save back state
-	    thisActor->start=start;
-	    thisActor->count=count;
-	    thisActor->comp=comp;
-	    result = exitcode_block_In_1;
-	    goto out;
-	  }
-
-	  if (comp<4) {
-	    // The Y sub-macroblocks are stored interleaved
-	    // in the macroBlock buffer: a row (8 bytes) of Y0
-	    // is followed by a row of Y1 (same for Y2/Y3).
-	    // In the CAL code this is expressed as:
-	    //
-	    // if (comp<4) and (bitand(count, 7)=0) then
-	    //   start := start + 8;
-	    // end
-	    //
-	    // So we want to go to the next multiple of eight
-	    n=8-(count & 7);
-	  }
-	  else
-	    n=64-count;
-
-	  if (avail<n)
-	    n=avail;
-	  ptr=thisActor->macroBlock+RANGECHK(start+count, MB_SIZE);
-	  end=ptr+n;
-	  do {
-	    ART_ACTION_ENTER(read, 0);
-	    *ptr++=pinRead_int32_t(IN0_In);
-	    ART_ACTION_EXIT(read, 0);
-	  } while (ptr<end);
-
-	  count += n;
-	  if (comp<4 && (count & 7)==0)
-	    start=start+8;
+	if (avail<64) {
+	  // Save back state
+	  thisActor->count=count;
+	  result = exitcode_block_In_64;
+	  goto out;
 	}
 
-	/*** done.comp action ***/
-	count=0;
-	comp=comp+1;
-	start=comp*64;
-	if (comp==1 || comp==3)
-	  start-=56;  // due to the interleaving of Y0/Y1 and Y2/Y3
-      } while (comp<6);
+	ART_ACTION_ENTER(read, 0);
+	ptr=thisActor->macroBlock+count;
+	pinReadRepeat_int32_t(IN0_In,ptr,64);
+	ART_ACTION_EXIT(read, 0);
+	count += 64;
+      }
+
+      /*** done.comp action ***/
+      count=0;
 
       /*** done.mb action ***/
       thisActor->frameBuffer.display_yuv(thisActor->mbx,
 					 thisActor->mby,
 					 thisActor->macroBlock,
 					 &thisActor->frameBuffer);
-      comp=0;
-      start=0;
       thisActor->mbx+=16;
     } while (thisActor->mbx<thisActor->width);
     
@@ -206,21 +165,19 @@ out:
 } 
 
 static void art_Display_yuv_constructor(AbstractActorInstance *pBase) {
-	ActorInstance_art_Display_yuv	*thisActor=(ActorInstance_art_Display_yuv*) pBase;
-	struct timeb tb;
-	int width=thisActor->width;
-	int height=thisActor->height;
+  ActorInstance_art_Display_yuv	*thisActor=(ActorInstance_art_Display_yuv*) pBase;
+  struct timeb tb;
+  int width=thisActor->width;
+  int height=thisActor->height;
 
-	if (width==0 || height==0) {
-	  runtimeError(pBase, "Width and/or height parameter not set\n");
-	}
-	ftime(&tb);
-	memset(thisActor->macroBlock,0,MB_SIZE);	
-	thisActor->mbx=0;
-	thisActor->mby=0;
-	thisActor->count=0;
-	thisActor->comp=0;
-	thisActor->start=0;
+  if (width==0 || height==0) {
+    runtimeError(pBase, "Width and/or height parameter not set\n");
+  }
+  ftime(&tb);
+  memset(thisActor->macroBlock,0,MB_SIZE);	
+  thisActor->mbx=0;
+  thisActor->mby=0;
+  thisActor->count=0;
 	
   thisActor->startTime=tb.time*1000+tb.millitm;
   thisActor->now=thisActor->startTime;
@@ -269,20 +226,26 @@ static const PortDescription inputPortDescriptions[]={
   {"In", sizeof(int32_t)}
 };
 
+// TBD: Only used in RM-version (remove?)
 static const PortDescription outputPortDescriptions[]={
   {"Out", sizeof(int32_t)}
 };
 
-static const int portRate_1[] = {
-  1
+static const int portRate_64[] = {
+  64
 };
 
 static const int portRate_0[] = {
   0
 };
 
+// TBD: Only used in RM-version (remove?)
+static const int portRate_1[] = {
+  1
+};
+
 static const ActionDescription actionDescriptions[] = {
-  {"read", portRate_1, 0},
+  {"read", portRate_64, 0},
   {"done.comp", portRate_0, 0},
   {"done.mb", portRate_0, 0},
 #ifdef RM  
