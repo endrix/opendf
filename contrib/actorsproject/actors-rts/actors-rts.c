efcde9cd2e2169e8329437642d0a7c203e6dc74c
carlvp <carlvp@4d97daf9-442b-0410-bea3-872e0d7a45eb>
2010-11-29 10:00:54 +0000

    Removed false deadlock alarm in termination report

--- contrib/actorsproject/actors-rts/actors-rts.c
+++ contrib/actorsproject/actors-rts/actors-rts.c
@@ -1293,10 +1293,10 @@ int executeNetwork(int argc,
     }
   }
   if (result == 0) {
-    deadlock_report(runtime_data,numInstances,terminationReport);
+    if (buffer_report(runtime_data) || terminationReport)
+      deadlock_report(runtime_data,numInstances,terminationReport);
     run_destructors(runtime_data);
     show_result(runtime_data, show_statistics, show_timing);
-    buffer_report(runtime_data);
   }
   
   if (result==0 && generateFile) {
